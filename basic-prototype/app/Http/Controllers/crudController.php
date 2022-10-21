cca0a246cdaaf60cb1c61750dda8224fcf9092ad
Fatima Zahrae Acharki <fatimazahraeacharki14@gmail.com>
2022-10-21 15:16:06 +0100

    first commit

---
+++ basic-prototype/app/Http/Controllers/crudController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\promotion;
+
+class crudController extends Controller
+{
+    public function insert(Request $request){
+        $promo = new promotion;
+        $promo->name = $request->name;
+        $promo->save();
+        redirect('test');
+    }
+    public function show(){}
+}
---
+++ basic-prototype/routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\crudController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/test', function () {
+    return view('test');
+});
+
+Route::post('insert', [crudController::class, 'insert'])->name('insert');
